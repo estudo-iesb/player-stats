18dd2f16dabe81db7709a5ad85c97baa27eac946
Eduardo Caetano <eduardo.caetano@tecnisys.com.br>
2023-10-26 00:06:29 -0300

    ajuste page de search

--- src/screens/jogador/JogadorSearch.js
+++ src/screens/jogador/JogadorSearch.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Avatar, Card, IconButton, Searchbar} from 'react-native-paper'
+import { Avatar, Card, IconButton, Searchbar } from 'react-native-paper'
 import apiTheSports from '../../services/apiTheSports'
 import { ScrollView } from 'react-native'
 
@@ -10,44 +10,45 @@ const JogadorSearch = ({ navigation }) => {
 
   const fetchJogadores = async () => {
     try {
-        const response = await apiTheSports.get(`/3/searchplayers.php?p=${searchText}`);
-        // Filtra os jogadores para incluir apenas aqueles com strSport igual a "Soccer" (Futebol)
-        const jogadoresDeFutebol = response.data.player.filter((jogador) => jogador.strSport === "Soccer");
-        setJogadores(jogadoresDeFutebol);
+      const response = await apiTheSports.get(`/3/searchplayers.php?p=${searchText}`);
+      // Filtra os jogadores para incluir apenas aqueles com strSport igual a "Soccer" (Futebol)
+      const jogadoresDeFutebol = response.data.player.filter((jogador) => jogador.strSport === "Soccer");
+      setJogadores(jogadoresDeFutebol);
     } catch (error) {
-        console.error('Erro ao buscar os dadoss:', error);
+      console.error('Erro ao buscar os dadoss:', error);
     }
-}
+  }
 
   useEffect(() => {
     fetchJogadores()
   }, [searchText])
 
   return (
-    <ScrollView>
-
+    <>
       <Searchbar
         placeholder="Pesquisar Jogadores"
         onChangeText={(text) => setSearchText(text)}
         value={searchText}
       />
 
-      {jogadores.map(item => (
-        <Card key={item.idPlayer}>
-          <Card.Title
-            title={item.strPlayer}
-            subtitle={item.strTeam}
-            left={(props) => (
-              <Avatar.Image
-                size={44}
-                source={{ uri: item.strThumb }}
-              />
-            )}
-            right={(props) => <IconButton {...props} icon="chevron-right" onPress={() => navigation.push('JogadorProfile', { id: item.idPlayer })} />}
-          />
-        </Card>
-      ))}
-    </ScrollView>
+      <ScrollView>
+        {jogadores.map(item => (
+          <Card key={item.idPlayer}>
+            <Card.Title
+              title={item.strPlayer}
+              subtitle={item.strTeam}
+              left={(props) => (
+                <Avatar.Image
+                  size={44}
+                  source={{ uri: item.strThumb }}
+                />
+              )}
+              right={(props) => <IconButton {...props} icon="chevron-right" onPress={() => navigation.push('JogadorProfile', { id: item.idPlayer })} />}
+            />
+          </Card>
+        ))}
+      </ScrollView>
+    </>
   )
 }
 
